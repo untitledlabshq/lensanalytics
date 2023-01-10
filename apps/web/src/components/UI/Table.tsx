f800be280f870877b413cca1cfcd9c8dd3ecb26c
saurabh <saurabh@addresszero.org>
2023-01-10 12:24:30 +0530

    feat: init UI component

---
+++ apps/web/src/components/UI/Table.tsx
@@ -0,0 +1,60 @@
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import type { FC } from "react";
+
+interface Props {
+  columns: any;
+  data: any;
+}
+
+const Table: FC<Props> = ({ columns, data }) => {
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  return (
+    <div className="rounded-lg border border-slate-200">
+      <table className="border-collapse table-auto w-full bg-white text-sm rounded-lg overflow-hidden">
+        <thead className="bg-slate-50">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="border-slate-200 font-semibold p-4 text-slate-900 text-left"
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} className="p-4 border-b border-slate-50">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
