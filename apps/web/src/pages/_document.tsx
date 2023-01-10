48af74ecc85e21c4353ad90f716442a1a28495b3
saurabh <saurabh@addresszero.org>
2023-01-10 12:13:28 +0530

    chore: added favicon

---
+++ apps/web/src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class LensAnalyticsDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          {/* Favicon */}
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default LensAnalyticsDocument;
