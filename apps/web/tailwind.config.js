9faced0f7cc28635ac4ae22bf8e529928e3ecc8b
saurabh <saurabh@addresszero.org>
2023-01-10 12:05:01 +0530

    chore: extended colors

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00501E",
+        secondary: "#ABFE2C",
+      },
+    },
+  },
+  plugins: [],
+};
