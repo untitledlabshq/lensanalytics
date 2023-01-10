bed7c3fe7d66155f0c4e6106b74e9499e054537b
saurabh <saurabh@addresszero.org>
2023-01-10 12:09:23 +0530

    chore: added application constants

---
+++ apps/web/src/constants.ts
@@ -0,0 +1,15 @@
+// lens api endpoints
+export const MAINNET_API_URL = "https://api.lens.dev";
+
+// Application
+export const APP_NAME = "Lens Analytics";
+export const DEFAULT_OG =
+  "https://res.cloudinary.com/dussxn7wr/image/upload/v1673098363/Group_1_1_n1zv8m.jpg";
+export const DESCRIPTION =
+  "Lens Analytics allows you to explore and search the lens profiles and it's activities 🌿";
+
+// URLs
+export const IPFS_GATEWAY = "https://lens.infura-ipfs.io/ipfs/";
+
+// Sources
+export const LENS_SOURCES = ["phaver", "lenster", "lentube", "orb", "lensport"];
