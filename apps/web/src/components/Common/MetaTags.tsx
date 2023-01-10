9945c54590fee1ec08c4b2b49a957a138c772ac6
saurabh <saurabh@addresszero.org>
2023-01-10 12:19:59 +0530

    chore: added twitter card support

---
+++ apps/web/src/components/Common/MetaTags.tsx
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import type { FC } from "react";
+
+import { APP_NAME, DEFAULT_OG, DESCRIPTION } from "../../constants";
+
+interface Props {
+  title?: string;
+  description?: string;
+}
+
+const MetaTags: FC<Props> = ({
+  title = "Lens Analytics | Lens Protocol Explorer",
+  description = DESCRIPTION,
+}) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=5, viewport-fit=cover"
+      />
+
+      <meta property="og:url" content="https://lensanalytics.xyz/" />
+      <meta property="og:site_name" content={APP_NAME} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={DEFAULT_OG} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="400" />
+
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:site" content={APP_NAME} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image:src" content={DEFAULT_OG} />
+      <meta property="twitter:image:width" content="400" />
+      <meta property="twitter:image:height" content="400" />
+      <meta property="twitter:creator" content="addresszerohq" />
+
+      <link
+        rel="search"
+        type="application/opensearchdescription+xml"
+        href="/opensearch.xml"
+        title={APP_NAME}
+      />
+    </Head>
+  );
+};
+
+export default MetaTags;
