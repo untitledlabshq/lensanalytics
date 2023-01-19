9c71498b3ef26c6df325068416fed1709c6fb0be
saurabh <saurabh@addresszero.org>
2023-01-19 19:18:44 +0530

    ft: link logo to homepage

--- apps/web/src/components/Shared/Navbar/index.tsx
+++ apps/web/src/components/Shared/Navbar/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import Search from "./Search";
 
 const Navbar = () => {
@@ -5,18 +7,20 @@ const Navbar = () => {
     <div className="w-full border-b border-slate-100 py-4 px-4 bg-white">
       <div className="w-full md:max-w-6xl mx-auto flex items-center justify-between">
         {/* Logo */}
-        <div className="flex items-end space-x-2">
-          <img
-            src="/logo.svg"
-            alt="Logo"
-            className="w-5 h-5"
-            width={24}
-            height={24}
-          />
-          <h1 className="text-lg text-slate-900 font-semibold leading-none">
-            Lens Analytics
-          </h1>
-        </div>
+        <Link href={"/"}>
+          <div className="flex items-end space-x-2">
+            <img
+              src="/logo.svg"
+              alt="Logo"
+              className="w-5 h-5"
+              width={24}
+              height={24}
+            />
+            <h1 className="text-lg text-slate-900 font-semibold leading-none">
+              Lens Analytics
+            </h1>
+          </div>
+        </Link>
         {/* Search Bar */}
         <div className="flex-1 w-full md:max-w-[30%]">
           <Search />
